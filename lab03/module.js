a82c726d679f64cb10f4bbda0644927fb434a650
Cameron Dewey <cjd38@gold06.cs.calvin.edu>
2018-11-07 14:47:32 -0500

    started lab09, needs finished and to talk with prof. Did lab03

---
+++ lab03/module.js
@@ -0,0 +1,11 @@
+/**
+ * Sample NodeJS module
+ */
+"use strict";
+
+let my_function = () => {
+    return "Hello, module function!";
+};
+
+module.exports.my_function = my_function;
+
---
+++ lab03/server.js
@@ -0,0 +1,31 @@
+/**
+ * Sample (Node.js, non-Express) webserver for CS 336, Unit 3 - Run with either:
+ *    node script.js
+ *    npm start
+ */
+"use strict";
+
+const http = require("http");
+const http_status = require("http-status-codes");
+
+const HOST = "127.0.0.1";
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/hello") {
+        res.writeHead(http_status.OK, {"Content-Type": "text/plain"});
+        res.end("Hello, Node.js!");
+    } 
+    else if (req.url ==="/run") {
+    	res.use(express.static('public'));
+    }
+    else {
+        res.writeHead(http_status.NOT_FOUND, {"Content-Type": "text/plain"});
+        res.end();
+    }
+});
+
+server.listen(PORT, HOST, () => {
+    console.log("Server running on " + HOST + ":" + PORT + ".");
+});
+
