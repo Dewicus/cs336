47e02d244ea62d448a19ef9e25def63663763f8d
Cameron Dewey <cjd38@gold06.cs.calvin.edu>
2018-11-14 16:03:38 -0500

    weird error

--- lab09/app/scripts/CommentForm.js
+++ lab09/app/scripts/CommentForm.js
@@ -1,49 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Remarkable from 'remarkable';
 import $ from 'jquery';
 
-import '../css/base.css';
-
-import Comment from './Comment';
-
-var CommentForm = React.createClass({
-  getInitialState: function() {
-    return {author: '', text: ''};
-  },
-  handleAuthorChange: function(e) {
-    this.setState({author: e.target.value});
-  },
-  handleTextChange: function(e) {
-    this.setState({text: e.target.value});
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var author = this.state.author.trim();
-    var text = this.state.text.trim();
-    if (!text || !author) {
-      return;
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {author: '', text: ''};
+    },
+    handleAuthorChange: function(e) {
+        this.setState({author: e.target.value});
+    },
+    handleTextChange: function(e) {
+        this.setState({text: e.target.value});
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if (!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({author: author, text: text});
+        this.setState({author: '', text: ''});
+    },
+    render: function() {
+        return (
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input className="ui-widget ui-corner-all" type="text" placeholder="name..."
+                    value={this.state.author} onChange={this.handleAuthorChange}
+                />
+                <input className="ui-widget ui-corner-all" type="text" placeholder="comment..."
+                    value={this.state.text} onChange={this.handleTextChange}
+                />
+                <input className="ui-button ui-widget ui-corner-all" type="submit" value="Post" />
+            </form>
+        );
     }
-    this.props.onCommentSubmit({author: author, text: text});
-    this.setState({author: '', text: ''});
-  },
-  render: function() {
-    return (
-      <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          placeholder="Your name"
-          value={this.state.author}
-          onChange={this.handleAuthorChange}
-        />
-        <input
-          type="text"
-          placeholder="Say something..."
-          value={this.state.text}
-          onChange={this.handleTextChange}
-        />
-        <input type="submit" value="Post" />
-      </form>
-    );
-  }
 });
--- lab09/app/scripts/index.js
+++ lab09/app/scripts/index.js
@@ -1,14 +1,12 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Remarkable from 'remarkable';
-import $ from 'jquery';
+
+import CommentBox from './CommentBox';
 
 import '../css/base.css';
 
-
-// Put your JavaScript code here (rather than in the HTML file).
-
 ReactDOM.render(
-  <CommentBox url="/api/comments" pollInterval={2000} />,
-  document.getElementById('content')
+    <CommentBox url="/api/comments" pollInterval={2000}/>,
+    document.getElementById('content')
 );
