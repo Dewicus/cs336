6713c907987e9316c82ca13e0c274afb1d23a595
Cameron Dewey <cjd38@gold06.cs.calvin.edu>
2018-11-20 18:19:59 -0500

    readded lab07

---
+++ lab07/script-jquery.js
@@ -0,0 +1,33 @@
+"use strict";
+
+$(document).ready(function () {
+    $("#faqs h2").click(
+        function () {
+            $(this).next("div").toggleClass("open");
+            $(this).toggleClass("minus");
+        }
+    );
+    $("input#hello").click(
+        function () {
+            if ($(this).hasClass("minus")) {
+                console.log('AJAX request issued...');
+                // jQuery/AJAX deferred is similar to JavaScript promises, but we
+                // cast it to a standard promise/A+ in this example.
+                let jsPromise = Promise.resolve($.ajax({
+                    url: "/hello",
+                    type: "GET",
+                    data: {
+                        name: "jQuery-AJAX"
+                    }
+                }));
+                jsPromise.then(function (result) {
+                    console.log('AJAX request succeeded...');
+                    $("#hello").next("div").html("<p>" + result.content + "</p>");
+                }, function (xhr) {
+                    console.log('AJAX request failed...');
+                    $("#hello").next("div").html("<p>" + xhr.statusText + "</p>");
+                })
+            }
+        }
+    );
+});
