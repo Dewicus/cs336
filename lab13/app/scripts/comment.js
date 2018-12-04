211091cb38b08926fb7589228e0938f4c1463155
Cameron Dewey <cjd38@gold16.cs.calvin.edu>
2018-12-04 18:08:46 -0500

    edited lab13

--- lab13/app/scripts/comment.js
+++ lab13/app/scripts/comment.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import Remarkable from 'remarkable';
+import {Link} from 'react-router';
 
-// tutorial4.js
-var Comment = React.createClass({
-    rawMarkup: function() {
-        var md = new Remarkable();
+module.exports = React.createClass({
+    rawMarkup: function () {
+        var md = new Remarkable({html: true});
         var rawMarkup = md.render(this.props.children.toString());
-        return { __html: rawMarkup };
+        return {__html: rawMarkup};
     },
-
-    render: function() {
-        var md = new Remarkable();
+    render: function () {
         return (
             <div className="comment">
-		<h2 className="commentAuthor">
-		    {this.props.author}
-		</h2>
-		<span dangerouslySetInnerHTML={this.rawMarkup()} />
-        <Link to={'/' + this.props.id}>Edit</Link>
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()}/>
+                <Link to={'/' + this.props.id}>Edit</Link>
             </div>
         );
     }
 });
-
-module.exports = Comment;
